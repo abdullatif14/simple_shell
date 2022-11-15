999d27b6ce88385bd90bbaf3f2edc9b396e713ed
Lornzyy <lornanish@gmail.com>
2022-11-14 21:06:00 -0800

    main.c file

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * main - execute code
+ * @ac: ags count
+ * @argv: the argiments preseny
+ * Return: int
+ */
+
+int main(int ac, char **argv)
+{
+	char *prompt = "~$ ";
+	char *lineptr;
+	size_t n = 0;
+
+	(void)ac; (void)argv;
+	printf("%s", prompt);
+	getline(&lineptr, &n, stdin);
+	printf("%s", lineptr);
+
+	free(lineptr);
+	return (0);
+}
+
--- main.h
+++ main.h
@@ -1,10 +1,12 @@
-#ifndef _MAIN_H
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+
 
 #endif /*main_h*/
