1c36e612243b16b3aea64d2aa99996f3cc7b61fb
Abdilatif Mohamed <64832181+abdullatif14@users.noreply.github.com>
2022-11-16 22:45:40 +0300

    Update vars.c

--- vars.c
+++ vars.c
@@ -41,7 +41,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
  * @buf: the char buffer
  * @p: address of current position in buf
  * @i: starting position in buf
- * @len: length of buf
+ * @len: length of bufffer
  *
  * Return: Void
  */
